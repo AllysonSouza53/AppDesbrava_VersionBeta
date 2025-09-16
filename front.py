41915e0ef0ce1bdbcff3770f0dd622c812b20285
allyson_souza <souzaallyson53@gmail.com>
2025-09-15 23:15:10 -0300

    Adicionando o front

--- front.py
+++ front.py
@@ -1,17 +1,16 @@
 from Helpers import ManipulacaoArquivos
 import time as t
-
+"""
 ManipulacaoArquivos.escrever_arquivo(
     "AI\\SET\\memoria.txt",
     "ALUNOS\n"
     "RE,NOME,DATANASCIMENTO,NUMEROCASA,RUA,BAIRRO,IDCIDADE,SERIE,GRAU,IDESCOLA,IDPROFISSIONAL,UF\n"
     "12345,João,2008-05-10,100,Rua A,Centro,3550308,1,3,1,1,SP\n"
     "RE=12345\n"
-    "I\n"
+    "I"
 )
 
 t.sleep(5)
-"""
 ManipulacaoArquivos.escrever_arquivo(
     'AI\\SET\\memoria.txt',
     "BACKUP\n"
@@ -21,3 +20,5 @@ ManipulacaoArquivos.escrever_arquivo(
     "*"
 )
 """
+
+t.sleep(20)
